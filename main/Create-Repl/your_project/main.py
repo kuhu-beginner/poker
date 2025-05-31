492bb06f492ecc3e4f7cffaed15f6c60bbecdeac
kuhu-beginner <123246714+kuhu-beginner@users.noreply.github.com>
2025-05-31 22:55:24 +0900

    Update main.py

--- main/Create-Repl/your_project/main.py
+++ main/Create-Repl/your_project/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, join_room, leave_room
-from your_game_module import PokerGame
+from poker_game import PokerGame
 import uuid
 import eventlet
 
