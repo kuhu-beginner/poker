b8573c78236fa7abeeb674a5cd455c296db5dc44
kuhu-beginner <123246714+kuhu-beginner@users.noreply.github.com>
2025-05-31 22:42:12 +0900

    Update main.py

--- main/Create-Repl/your_project/main.py
+++ main/Create-Repl/your_project/main.py
@@ -4,18 +4,13 @@ import uuid
 import eventlet
 eventlet.monkey_patch()
 
-from flask import Flask
-app = Flask(__name__)
-
-from flask import Flask
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key-here'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-
 # Renderでの参照用
 application = app
+
 
 
                         # ゲームルームを管理
